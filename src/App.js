a3342a207d0447d0cebbf82f1270752bf33799ee
pguyon <pguyon69@gmail.com>
2022-09-09 02:40:35 +0200

    start verticalNav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HorizontalNav from './components/navbar/horizontalNav/HorizontalNav';
+import VerticalNav from './components/navbar/verticalNav/VerticalNav';
 
 function App() {
   return (
     <div className="App">
       <HorizontalNav />
+      <VerticalNav />
     </div>
   );
 }
--- src/components/navbar/verticalNav/VerticalNav.js
+++ src/components/navbar/verticalNav/VerticalNav.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './VerticalNav.css';
+import Yoga from '../../../assets/yoga.png'
+import Natation from '../../../assets/natation.png'
+import Cycling from '../../../assets/cycling.png'
+import Workout from '../../../assets/workout.png'
 
 const VerticalNav = () => {
+    const icons = [
+        Yoga,
+        Natation,
+        Cycling,
+        Workout
+    ]
     return (
-        <div>
-            
-        </div>
+        <aside>
+            {icons.map((icon, index) => (
+                <img key={index} src={icon} alt='' />
+            ))}
+            <span>Copyright, SportSee 2020</span>
+        </aside>
     );
 }
 
