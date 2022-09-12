e989998a39f0016095a3bd414645167a5c3b19fc
pguyon <pguyon69@gmail.com>
2022-09-12 06:36:24 +0200

    finish performance component

--- src/components/performance/Performance.js
+++ src/components/performance/Performance.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Performance.css";
 import {
   Radar,
@@ -8,62 +8,62 @@ import {
   PolarRadiusAxis,
   ResponsiveContainer,
 } from "recharts";
+import { getUserPerformance } from "../../services/Api";
+import { userDataPerformanceModel } from "../../services/UserDataModel";
 
-const data = [
-  {
-    subject: "Math",
-    A: 120,
-    B: 110,
-    fullMark: 150,
-  },
-  {
-    subject: "Chinese",
-    A: 98,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: "English",
-    A: 86,
-    B: 130,
-    fullMark: 150,
-  },
-  {
-    subject: "Geography",
-    A: 99,
-    B: 100,
-    fullMark: 150,
-  },
-  {
-    subject: "Physics",
-    A: 85,
-    B: 90,
-    fullMark: 150,
-  },
-  {
-    subject: "History",
-    A: 65,
-    B: 85,
-    fullMark: 150,
-  },
-];
+const Performance = ({ userId }) => {
+  const [userDataPerformance, setUserDataPerformance] = useState({});
+  const [isLoading, setIsloading] = useState(false);
 
-const Performance = () => {
-  return (
-    <ResponsiveContainer width="100%" height="100%">
-      <RadarChart
-        cx="50%"
-        cy="50%"
-        outerRadius="80%"
-        data={data}
-        className="radar">
-        <PolarGrid />
-        <PolarAngleAxis dataKey="subject" />
-        <PolarRadiusAxis />
-        <Radar name="Mike" dataKey="A" fill="red" fillOpacity={0.6} />
-      </RadarChart>
-    </ResponsiveContainer>
-  );
+  useEffect(() => {
+    getUserPerformance(userId).then((response) => {
+      const formattedUserDataPerformance = new userDataPerformanceModel(
+        response.data
+      );
+      setUserDataPerformance(formattedUserDataPerformance);
+      setIsloading(true);
+      return response.data;
+    });
+  }, [userId]);
+
+  const activities = userDataPerformance?.kind;
+  const userPerformance = userDataPerformance?.data;
+
+  const formattedData = userPerformance?.map((item) => {
+    return {
+      activity: activities[item.kind],
+      value: item.value,
+    };
+  });
+
+  // console.log(activities);
+  console.log(userPerformance);
+
+  if (isLoading) {
+    return (
+      <ResponsiveContainer width="100%" height="100%">
+        <RadarChart
+          cx="50%"
+          cy="50%"
+          outerRadius="80%"
+          data={formattedData.reverse()}
+          className="radar">
+          <PolarGrid />
+          <PolarAngleAxis
+            dataKey="activity"
+            stroke="#FFF"
+            tickLine={false}
+            tick={{
+              fontSize: 8,
+              fontWeight: 500,
+            }}
+          />
+          <PolarRadiusAxis />
+          <Radar dataKey="value" fill="red" fillOpacity={0.6} />
+        </RadarChart>
+      </ResponsiveContainer>
+    );
+  }
 };
 
 export default Performance;
