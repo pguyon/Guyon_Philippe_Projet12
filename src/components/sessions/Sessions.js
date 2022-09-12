be96eee09538b1fa5833c9d267f83ffc3666b027
pguyon <pguyon69@gmail.com>
2022-09-12 14:08:59 +0200

    update css

--- src/components/sessions/Sessions.js
+++ src/components/sessions/Sessions.js
@@ -35,54 +35,60 @@ const Sessions = ({ userId }) => {
 
   if (isLoading) {
     return (
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          className="container"
-          data={userDataSession.sessions}
-          margin={{ top: 15, left: 15, right: 15, bottom: 10 }}>
-          <XAxis
-            dataKey="day"
-            stroke="#FFF"
-            tickLine={false}
-            axisLine={false}
-            tick={{
-              fontSize: 12,
-              fontWeight: 500,
-            }}
-            tickFormatter={(item) => {
-              const days = ["L", "M", "M", "J", "V", "S", "D"];
-              return `${days[item - 1]}`;
-            }}
-          />
-          <YAxis hide={true} padding={{ top: 80, bottom: 30 }} />
-          <Tooltip
-            content={<CustomTooltip />}
-            cursor={{
-              stroke: "#000",
-              strokeOpacity: 0.1,
-              strokeWidth: 40,
-            }}
-            labelStyle={{
-              display: "none",
-            }}
-            itemStyle={{
-              color: "#000",
-            }}
-          />
-          <Line
-            type="monotone"
-            dataKey="sessionLength"
-            stroke="#FFF"
-            strokeWidth={1.5}
-            dot={false}
-            activeDot={{
-              stroke: "#FFF",
-              strokeOpacity: 0.4,
-              strokeWidth: 10,
-            }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+      <section>
+        <h3 className="sessions__title">
+          Durée moyenne des <br />
+          sessions
+        </h3>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            className="container"
+            data={userDataSession.sessions}
+            margin={{ top: 15, left: 15, right: 15, bottom: 10 }}>
+            <XAxis
+              dataKey="day"
+              stroke="#FFF"
+              tickLine={false}
+              axisLine={false}
+              tick={{
+                fontSize: 12,
+                fontWeight: 500,
+              }}
+              tickFormatter={(item) => {
+                const days = ["L", "M", "M", "J", "V", "S", "D"];
+                return `${days[item - 1]}`;
+              }}
+            />
+            <YAxis hide={true} padding={{ top: 80, bottom: 30 }} />
+            <Tooltip
+              content={<CustomTooltip />}
+              cursor={{
+                stroke: "#000",
+                strokeOpacity: 0.1,
+                strokeWidth: 40,
+              }}
+              labelStyle={{
+                display: "none",
+              }}
+              itemStyle={{
+                color: "#000",
+              }}
+            />
+            <Line
+              type="monotone"
+              dataKey="sessionLength"
+              stroke="#FFF"
+              strokeWidth={1.5}
+              dot={false}
+              activeDot={{
+                stroke: "#FFF",
+                strokeOpacity: 0.4,
+                strokeWidth: 10,
+              }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </section>
     );
   }
 };
