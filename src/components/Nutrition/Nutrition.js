6aa39e73fe404f1bd2e06fb8802686f3560d59de
pguyon <pguyon69@gmail.com>
2022-09-16 11:33:41 +0200

    add comma on keayData kCal

--- src/components/Nutrition/Nutrition.js
+++ src/components/Nutrition/Nutrition.js
@@ -7,18 +7,25 @@ import Carb from "../../assets/carbs-icon.png";
 import Protein from "../../assets/protein-icon.png";
 
 const Nutrition = ({ keyData }) => {
-  console.log(keyData);
-  console.log(keyData.calorieCount);
-  console.log(keyData.proteinCount);
-  console.log(keyData.carbohydrateCount);
-  console.log(keyData.lipidCount);
+  //   console.log(keyData);
+  //   console.log(keyData.calorieCount);
+  //   console.log(keyData.proteinCount);
+  //   console.log(keyData.carbohydrateCount);
+  //   console.log(keyData.lipidCount);
+
+  const cal =
+    keyData.calorieCount.toString().substr(0, 1) +
+    "," +
+    keyData.calorieCount.toString().substr(1, 5);
+
+  console.log(cal);
 
   return (
     <div>
       <div className="nutrition__wrapper">
         <img src={Calories} alt="calories icon" />
         <div className="nutrition__details">
-          <span className="nutrition__data">{keyData.calorieCount}kCal</span>
+          <span className="nutrition__data">{cal}kCal</span>
           <span className="nutrition__name">Calories</span>
         </div>
       </div>
