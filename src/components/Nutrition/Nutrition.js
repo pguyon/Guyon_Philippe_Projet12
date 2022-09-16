20b9b952393e18df406ab0f88968099250d3ad6d
pguyon <pguyon69@gmail.com>
2022-09-16 12:03:55 +0200

    update css

--- src/components/Nutrition/Nutrition.js
+++ src/components/Nutrition/Nutrition.js
@@ -7,10 +7,6 @@ import Carb from "../../assets/carbs-icon.png";
 import Protein from "../../assets/protein-icon.png";
 
 const Nutrition = ({ keyData }) => {
-  //   console.log(keyData);
-  //   console.log(keyData.calorieCount);
-  //   console.log(keyData.proteinCount);
-  //   console.log(keyData.carbohydrateCount);
   //   console.log(keyData.lipidCount);
 
   const cal =
@@ -18,18 +14,37 @@ const Nutrition = ({ keyData }) => {
     "," +
     keyData.calorieCount.toString().substr(1, 5);
 
-  console.log(cal);
-
   return (
-    <div>
-      <div className="nutrition__wrapper">
+    <React.Fragment>
+      <div className="nutrition__content">
         <img src={Calories} alt="calories icon" />
         <div className="nutrition__details">
           <span className="nutrition__data">{cal}kCal</span>
           <span className="nutrition__name">Calories</span>
         </div>
       </div>
-    </div>
+      <div className="nutrition__content">
+        <img src={Protein} alt="protein icon" />
+        <div className="nutrition__details">
+          <span className="nutrition__data">{keyData.proteinCount}g</span>
+          <span className="nutrition__name">Protéines</span>
+        </div>
+      </div>
+      <div className="nutrition__content">
+        <img src={Carb} alt="glucid icon" />
+        <div className="nutrition__details">
+          <span className="nutrition__data">{keyData.carbohydrateCount}g</span>
+          <span className="nutrition__name">Glucides</span>
+        </div>
+      </div>
+      <div className="nutrition__content">
+        <img src={Fat} alt="lipid icon" />
+        <div className="nutrition__details">
+          <span className="nutrition__data">{keyData.lipidCount}g</span>
+          <span className="nutrition__name">Lipides</span>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
