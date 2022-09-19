b546c2e36f67c96e72dbb2076aa3ebf22c19a8f5
pguyon <pguyon69@gmail.com>
2022-09-19 14:53:15 +0200

    update docs

--- src/services/UserDataModel.js
+++ src/services/UserDataModel.js
@@ -5,7 +5,8 @@ creation of class to control the data retrieved with the requests
 /**
 * The userDataModel class is a constructor function that takes a data object as an argument and
 assigns the properties of that object to the userDataModel instance.
-* @param {Object} - id, userInfos score and keyData
+* @param {Object} data - id, userInfos todayScore and keyData
+* @type {{id: number, userInfos: object, todayScore: number, keyData: object}}
 * @author Philippe Guyon
 * @version 1.0
 */
@@ -22,6 +23,13 @@ export class userDataModel {
   }
 }
 
+/** The class takes in an object with a userId and sessions property. The sessions property is an array
+of objects. The class then adds a day property to each object in the sessions array
+* @param {Object} - id, sessions
+* @type {{userId: number, sessions: array}}
+* @author Philippe Guyon
+* @version 1.0
+*/
 export class userDataActivityModel {
   constructor(data) {
     this.userId = data.userId;
@@ -32,6 +40,14 @@ export class userDataActivityModel {
   }
 }
 
+/** The userDataPerformanceModel class is a constructor function that takes a single argument, data, and
+assigns the values of data.userId, data.kind, and data.data to the properties of the same name on
+the object that is created when the function is called.
+* @param {Object} - userId, kind, data
+* @type {{userId: number, kind: object, data: array}}
+* @author Philippe Guyon
+* @version 1.0
+*/
 export class userDataPerformanceModel {
   constructor(data) {
     this.userId = data.userId;
@@ -40,6 +56,12 @@ export class userDataPerformanceModel {
   }
 }
 
+/** This class is a model for the userDataAverageModel class.
+ *  @param {Object} - userId, sessions
+ *  @type {{userId: number, sessions: array}}
+ * @author Philippe Guyon
+ * @version 1.0
+ */
 export class userDataAverageModel {
   constructor(data) {
     this.userId = data.userId;
